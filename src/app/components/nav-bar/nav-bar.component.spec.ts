83a45a4c0c5a4a1e1d60a058b15d29040089452e
HanifaAbda <hanifabda2@gmail.com>
2025-06-16 10:01:24 +0200

    tailwind, navbar

--- src/app/components/nav-bar/nav-bar.component.spec.ts
+++ src/app/components/nav-bar/nav-bar.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavBarComponentComponent } from './nav-bar-component.component';
+import { NavBarComponent } from './nav-bar.component';
 
-describe('NavBarComponentComponent', () => {
-  let component: NavBarComponentComponent;
-  let fixture: ComponentFixture<NavBarComponentComponent>;
+describe('NavBarComponent', () => {
+  let component: NavBarComponent;
+  let fixture: ComponentFixture<NavBarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavBarComponentComponent]
+      imports: [NavBarComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(NavBarComponentComponent);
+    fixture = TestBed.createComponent(NavBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
